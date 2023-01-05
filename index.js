da414e375cea11e8ed905e1bace9303a5e0f0f53
Rati3010 <mratikanta1997@gmail.com>
2023-01-05 09:22:34 +0530

    pushed

---
+++ index.js
@@ -0,0 +1,9 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const  router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = 4001;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
