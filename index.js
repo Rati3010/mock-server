424e4b4eff86a9042a2da33bf645014a590ef0ef
Rati3010 <mratikanta1997@gmail.com>
2023-01-11 09:40:27 +0530

    pushed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const  router = jsonServer.router('db.json');
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults();
 const port = 4001;
 server.use(jsonServer.bodyParser)
